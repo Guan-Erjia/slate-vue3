573deabdda020aa7ea4bbf826dcd7cd8fbbc255b
guanjianzhou <guanjianzhou@baidu.com>
2025-01-23 16:54:26 +0800

    pack: 添加命令

---
+++ scripts/build.js
@@ -0,0 +1,13 @@
+import fs from "fs-extra";
+
+const task = async () => {
+  await fs.remove("./packages/slate/dist");
+  await fs.remove("./packages/slate-dom/dist");
+  await fs.remove("./packages/slate-vue/dist");
+  await fs.remove("./packages/un-proxy-weakmap/dist");
+  await fs.remove("./packages/slate/tsconfig.tsbuildinfo");
+  await fs.remove("./packages/slate-dom/tsconfig.tsbuildinfo");
+  await fs.remove("./packages/slate-vue/tsconfig.tsbuildinfo");
+  await fs.remove("./packages/un-proxy-weakmap/tsconfig.tsbuildinfo");
+};
+task();
