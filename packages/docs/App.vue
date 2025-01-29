a3668d72e7cc2f66504036df1ce68896dc1b42de
guanjianzhou <1357023518@qq.com>
2025-01-29 11:14:48 +0800

    docs: 修改文档入口

--- packages/docs/App.vue
+++ packages/docs/App.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
-import RichTextExample from './pages/richtext/index.vue';
-import Checklists from './pages/check-lists/index.vue';
+import RichText from './pages/richtext/index.vue';
+import CheckLists from './pages/check-lists/index.vue';
 import EditableVoids from './pages/editable-voids/index.vue';
 import Embeds from './pages/embeds/index.vue';
+import { computed } from 'vue';
+
+const ExampleOptions = computed(() => [
+  {
+    name: 'rich-text',
+    component: RichText,
+  },
+  {
+    name: 'check-lists',
+    component: CheckLists,
+  },
+  {
+    name: 'editable-voids',
+    component: EditableVoids,
+  },
+  {
+    name: 'embeds',
+    component: Embeds,
+  }
+])
 </script>
 
 <template>
@@ -12,33 +32,35 @@ import Embeds from './pages/embeds/index.vue';
     <a href="https://github.com/Guan-Erjia/slate-vue3" target="_blank">GitHub</a>
   </header>
   <div style="display: flex;height: calc(100% - 50px);">
-    <ol class="nav-list">
-      <a href="#RichText">
-        <li class="nav-list-item">Rich Text</li>
-        <li class="nav-list-item">Checklists</li>
-        <li class="nav-list-item">EditableVoids</li>
+    <ol style="width: 160px;flex-shrink: 0;box-sizing: border-box;">
+      <a :href="`#${item.name}`" v-for="item in ExampleOptions">
+        <li style="text-decoration: none;margin-bottom: 10px;">{{ item.name }}</li>
       </a>
     </ol>
     <div class="scroll-container">
-      <div class="example-item">
-        <RichTextExample />
-      </div>
-      <div class="example-item">
-        <Checklists />
-      </div>
-      <div class="example-item">
-        <EditableVoids />
-      </div>
-      <div class="example-item">
-        <Embeds />
-      </div>
+      <template v-for="item in ExampleOptions">
+        <header style="display: flex; padding-top: 20px;" :id="item.name">
+          <span style="cursor: default;font-size: 18px;"> {{ item.name }} </span>
+          <div style="flex-grow: 1;"></div>
+          <a target="_blank"
+            :href="`https://github.com/Guan-Erjia/slate-vue3/tree/master/packages/docs/pages/${item.name}`">
+            view code</a>
+        </header>
+        <div style="padding: 20px;margin-top: 10px;box-sizing: border-box;background-color: white;">
+          <component :is="item.component" />
+        </div>
+      </template>
     </div>
   </div>
 </template>
-<style>
+<style scoped>
 a {
-  color: #aaaaaa;
+  color: gray;
   text-decoration: none
+}
+
+a:hover {
+  color: #aaaaaa;
 }
 
 .header {
@@ -52,28 +74,12 @@ a {
   align-items: center;
 }
 
-.nav-list {
-  width: 160px;
-  flex-shrink: 0;
-  box-sizing: border-box;
-}
-
-.nav-list-item {
-  text-decoration: none;
-}
-
 .scroll-container {
   min-width: 0;
   flex-grow: 1;
-  padding: 20px;
+  padding: 0 20px 20px 20px;
   height: 100%;
+  overflow-y: auto;
   box-sizing: border-box;
-}
-
-.example-item {
-  padding: 20px;
-  box-sizing: border-box;
-  background-color: white;
-  margin-bottom: 20px;
 }
 </style>
