3a541e8e2e069c30d45d985d355b2d55a4d424d3
guanjianzhou <1357023518@qq.com>
2025-02-18 15:14:38 +0800

    test: 添加单元测试

---
+++ test/slate/transforms/index.spec.js
@@ -0,0 +1,18 @@
+import { withTest } from "../../utils.js";
+import { test, expect, describe } from "vitest";
+
+const modules = import.meta.glob("./**/*.(j|t)s?(x)");
+
+describe("slate-transforms", () => {
+  Object.keys(modules).forEach(async (path) => {
+    test(path, async () => {
+      const { input, run, output } = await modules[path]();
+
+      const editor = withTest(input);
+      run(editor);
+
+      expect(editor.children).toStrictEqual(output.children);
+      expect(editor.selection).toStrictEqual(output.selection);
+    });
+  });
+});
