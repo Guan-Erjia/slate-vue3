cbdd0a5e107818a5f693a67f7b3b9d1cf341b06a
guanjianzhou <1357023518@qq.com>
2025-02-21 18:05:52 +0800

    perf: 删除无关导入

--- packages/slate-vue/src/components/children.ts
+++ packages/slate-vue/src/components/children.ts
@@ -1,4 +1,4 @@
-import { Descendant, Element } from "slate";
+import { Element } from "slate";
 import { ElementComp } from "./element";
 import { TextComp } from "./text";
 import {
@@ -25,16 +25,17 @@ export const Children = defineComponent({
       IS_NODE_MAP_DIRTY.set(editor, false);
     });
 
-    return () => renderList(props.node.children, (child, i): VNode => {
-      // 这些逻辑不会触发多余渲染
-      const key = DOMEditor.findKey(editor, child);
-      // 组件直接传入索引将不会动态更新，必须通过 NODE_TO_INDEX 手动获取索引
-      NODE_TO_INDEX.set(child, i);
-      NODE_TO_PARENT.set(child, props.node);
+    return () =>
+      renderList(props.node.children, (child, i): VNode => {
+        // 这些逻辑不会触发多余渲染
+        const key = DOMEditor.findKey(editor, child);
+        // 组件直接传入索引将不会动态更新，必须通过 NODE_TO_INDEX 手动获取索引
+        NODE_TO_INDEX.set(child, i);
+        NODE_TO_PARENT.set(child, props.node);
 
-      return Element.isElement(child) ?
-        h(ElementComp, { element: child, key: key.id })
-        : h(TextComp, { text: child, parent: props.node, key: key.id })
-    })
+        return Element.isElement(child)
+          ? h(ElementComp, { element: child, key: key.id })
+          : h(TextComp, { text: child, parent: props.node, key: key.id });
+      });
   },
 });
