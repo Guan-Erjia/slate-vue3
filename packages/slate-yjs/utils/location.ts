b1b6b79e39bfee756937238c2c1ff0ae5d75c62d
guanjianzhou <1357023518@qq.com>
2025-03-24 23:21:53 +0800

    fix: 待修复指针问题-数组越界

--- packages/slate-yjs/utils/location.ts
+++ packages/slate-yjs/utils/location.ts
@@ -34,7 +34,9 @@ export function getYTarget(
   const [pathOffset, ...childPath] = path;
 
   const yOffset = slatePathOffsetToYOffset(slateRoot, pathOffset);
-  const targetNode = slateRoot.children[pathOffset];
+  // fixme 这里的逻辑有问题，pathOffset会超出范围， 不知道哪个地方的指针连在一起了
+  const slateRootChildrenLength = slateRoot.children.length;
+  const targetNode = slateRoot.children[pathOffset >= slateRootChildrenLength ? slateRootChildrenLength - 1 : pathOffset];
 
   const delta = yTextToInsertDelta(yRoot);
   const targetLength = getSlateNodeYLength(targetNode);
